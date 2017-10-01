2c9a3cc744103ef7d20785c07d8c91d80e165f67
drhie <rhie.david@gmail.com>
2017-10-01 13:56:38 +0900

    Remove old validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :spreadsheet, counter_cache: true, optional: true
   belongs_to :user, optional: true
   belongs_to :category
-  validates_presence_of :name, :amount, :item_type
+  validates_presence_of :name, :amount
 
 end
