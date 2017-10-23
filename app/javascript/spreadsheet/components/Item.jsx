15aa1bde0718b45b7f709c03080f36fc1c72567b
drhie <rhie.david@gmail.com>
2017-10-23 17:03:45 +0900

    Add date to item

--- app/javascript/spreadsheet/components/Item.jsx
+++ app/javascript/spreadsheet/components/Item.jsx
@@ -16,7 +16,10 @@ export default class Item extends React.Component {
       <div className={"border item-box " + this.props.className} onClick={()=>this.props.onEdit(this.props.item)}>
         <div className="delete" onClick={(e)=>{e.stopPropagation(); this.props.onDelete(this.props.item)}}>x</div>
         <p className="item-name">{this.props.item["name"]}</p>
-        <p className="item-amount">{formatInteger(this.props.currency, this.props.item["amount"])}</p>
+        <div className="item-bottom-half">
+          <p className="item-transaction-date">{this.props.item["transactionDate"]}</p>
+          <p className="item-amount">{formatInteger(this.props.currency, this.props.item["amount"])}</p>
+        </div>
       </div>
     )
   }
